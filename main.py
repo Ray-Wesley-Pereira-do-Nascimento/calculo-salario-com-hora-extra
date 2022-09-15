babeb2d37e83ba4d41a9d231033c11a80a3480e2
Ray Wesley Pereira do Nascimento <raywesleypereira@gmail.com>
2022-09-15 13:42:51 -0300

    Foi feito modificações para deixar o codigo mais limpo

--- main.py
+++ main.py
@@ -6,43 +6,22 @@
 """
 
 
-horas1 = -1
-horas2 = -1
-horas3 = -1
-horas4 = -1
-hora_extra1 = 0
-hora_extra2 = 0
-hora_extra3 = 0
-hora_extra4 = 0
-while horas1 < 0 or horas1 > 50:
-  horas1 = int(input(f'Digite a quantidade de horas da 1 semana: '))
-  if horas1 < 0 or horas1 > 50:
-     print('Quantidade de horas inválida! Digite novamente ou contate o RH.')
-  elif horas1 > 40 and horas1 <= 50:
-     hora_extra1 = horas1 - 40
-while horas2 < 0 or horas2 > 50:
-   horas2 = int(input(f'Digite a quantidade de horas da 2 semana: '))
-   if horas2 < 0 or horas2 > 50:
-    print('Quantidade de horas inválida! Digite novamente ou contate o RH.')
-   elif horas2 > 40 and horas2 <= 50:
-     hora_extra2 = horas2 - 40
-while horas3 < 0 or horas3 > 50:
-    horas3 = int(input(f'Digite a quantidade de horas da 3 semana: '))
-    if horas3 < 0 or horas3 > 50:
-     print('Quantidade de horas inválida! Digite novamente ou contate o RH.')
-    elif horas3 > 40 and horas3 <= 50:
-      hora_extra3 = horas3 - 40
-while horas4 < 0 or horas4 > 50:
-   horas4 = int(input(f'Digite a quantidade de horas da 4 semana: '))
-   if horas4 < 0 or horas4 > 50:
-    print('Quantidade de horas inválida! Digite novamente ou contate o RH.')
-   elif horas4 > 40 and horas4 <= 50:
-      hora_extra4 = horas4 - 40
+horas = [-1, -1, -1, -1]
+salario_semana = [0, 0, 0, 0]
 
-hora_extra = (hora_extra1 + hora_extra2 + hora_extra3 + hora_extra4)  * 0.5
-salario_hora = float(input('Digite o salário por hora: '))
-total_horas = horas1 + horas2 + horas3 + horas4
-total_salario = (hora_extra + total_horas) * salario_hora
-print(f"Total Horas Extras: {hora_extra}")
-print(f'Total de Horas: {total_horas}')
-print(F'O salário total do mês é de R${total_salario:.2f}')
+valor_hora = float(input("Digite o valor do salário por hora: "))
+for n_semana in range(4):
+  while horas[n_semana] < 0 or horas[n_semana] > 50:
+   horas[n_semana] = int(input(f"Digite a quantidade de horas trabalhadas na semana {n_semana + 1}: "))
+   if horas[n_semana] < 0 or horas[n_semana] > 50: print("Quantidade de horas inválida. Digite novamente: ")
+   else:
+     if horas[n_semana] <= 40:
+       salario_semana[n_semana] = valor_hora * horas[n_semana]
+     else:
+       salario_semana[n_semana] = valor_hora * 40
+       salario_semana[n_semana] += (horas[n_semana] - 40) * (valor_hora + valor_hora * 0.5)
+salario_mes = 0
+for n_semana in range(4):
+  salario_mes += salario_semana[n_semana]
+print(f"O salário do mês é de R$ {salario_mes}")
+
